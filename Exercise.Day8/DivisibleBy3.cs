36319c36a61ae77c4bc8092d8bed8c44ee2f0fa9
Samarth <samarathdk2001@gmail.com>
2022-03-16 12:19:19 +0530

    FirstCommit

---
+++ Exercise.Day8/DivisibleBy3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samarth.Exercise.Day8
+{
+    internal class DivisibleBy3
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Array Size :");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            WriteLine("Array Elements :");
+
+            for (int i = 0; i < n; i++)
+            {
+                int a = 0;
+                if (i % 3 == 0);
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+        }
+}
---
+++ Exercise.Day8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace Samarth.Day8.Exercise
+{
+    internal class LargestNumberQue
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Array Size :");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            WriteLine("Array Elements :");
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(arr);
+            Array.Reverse(arr);
+            WriteLine(String.Join("",arr));
+        }
+    }
+}
